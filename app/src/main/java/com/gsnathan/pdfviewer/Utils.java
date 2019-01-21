a5a205ea2437930ed13911e477c3eb5ff4832f67
Gokul Swami <gokulswamilive@gmail.com>
2019-01-20 19:51:08 -0800

    CI is might fail due to lack of translations

--- app/src/main/java/com/gsnathan/pdfviewer/Utils.java
+++ app/src/main/java/com/gsnathan/pdfviewer/Utils.java
@@ -100,21 +100,4 @@ public class Utils {
     static String getAppVersion() {
         return BuildConfig.VERSION_NAME;
     }
-
-    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
-        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            drawable = (DrawableCompat.wrap(drawable)).mutate();
-        }
-
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bitmap;
-    }
-
-
 }
