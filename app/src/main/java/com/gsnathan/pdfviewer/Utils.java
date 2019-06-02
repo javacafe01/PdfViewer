9c84c5c5aef4817e4653d156663a5cb2dd56e683
Gokul Swaminathan <gokulswamilive@gmail.com>
2019-06-02 16:58:33 -0700

    Final Touches

--- app/src/main/java/com/gsnathan/pdfviewer/Utils.java
+++ app/src/main/java/com/gsnathan/pdfviewer/Utils.java
@@ -49,8 +49,8 @@ public class Utils {
 
     static void showLog(AppCompatActivity context) {
         WhatsNew log = WhatsNew.newInstance(
-                new WhatsNewItem("Settings", "A lot of PDF rendering settings have been added.", R.drawable.thumbs_icon),
-                new WhatsNewItem("Menu", "Brand new menu that holds all the options.", R.drawable.star_icon));
+                new WhatsNewItem("Translation", "Added Russian localization", R.drawable.thumbs_icon),
+                new WhatsNewItem("Printing", "Implemented printing options", R.drawable.star_icon));
         log.setTitleColor(ContextCompat.getColor(context, R.color.colorAccent));
         log.setTitleText(context.getResources().getString(R.string.appChangelog));
         log.setButtonText(context.getResources().getString(R.string.buttonLog));
