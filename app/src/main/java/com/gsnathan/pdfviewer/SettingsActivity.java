e1b0a26a79f7962a58bc0211392d8c889faf9aeb
Fs00 <francescosaltori@gmail.com>
2020-12-24 14:19:12 +0100

    Remove unused imports

--- app/src/main/java/com/gsnathan/pdfviewer/SettingsActivity.java
+++ app/src/main/java/com/gsnathan/pdfviewer/SettingsActivity.java
@@ -1,18 +1,10 @@
 package com.gsnathan.pdfviewer;
 
+import android.app.ActionBar;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.app.ActionBar;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.MenuItem;
 
 import androidx.core.app.NavUtils;
