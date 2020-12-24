702b8f6119ca2941b998cb95312d1aaf5dcb0e21
Fs00 <francescosaltori@gmail.com>
2020-12-24 14:17:46 +0100

    Remove redundant Android API documentation

--- app/src/main/java/com/gsnathan/pdfviewer/SettingsActivity.java
+++ app/src/main/java/com/gsnathan/pdfviewer/SettingsActivity.java
@@ -21,17 +21,6 @@ import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
 import static android.content.pm.PackageManager.DONT_KILL_APP;
 
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
